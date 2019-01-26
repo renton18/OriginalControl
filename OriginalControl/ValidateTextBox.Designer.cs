0d1ea376ed538b3a6ec13b75c3a34f606b2d6b66
renton18 <gakituka18@gmail.com>
2019-01-27 01:09:28 +0900

    プロジェクト ファイルを追加します。

---
+++ OriginalControl/ValidateTextBox.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace OriginalControl
+{
+    partial class ValidateTextBox
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(0, 0);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(164, 19);
+            this.textBox.TabIndex = 0;
+            this.textBox.Validating += new System.ComponentModel.CancelEventHandler(this.textBox_Validating);
+            this.textBox.Validated += new System.EventHandler(this.textBox_Validated);
+            // 
+            // ValidateTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox);
+            this.Name = "ValidateTextBox";
+            this.Size = new System.Drawing.Size(186, 19);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
---
+++ OriginalControl/ValidateTextBox.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace OriginalControl
+{
+    public partial class ValidateTextBox: UserControl
+    {
+        public StringType stringType { get; set; } = StringType.HALF_NUMBER;
+
+        ErrorProvider errorProvider = new ErrorProvider();
+        public enum StringType
+        {
+            HALF_NUMBER,
+            HALF_NUMBER_DOT,
+            HALF_ALPHA_NUMBER,
+            HALF_ALPHA_NUMBER_SYMBOL
+        }
+        public ValidateTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_Validating(object sender, CancelEventArgs e)
+        {
+            string regexString = "";
+            string errorString = "";
+
+            switch (stringType)
+            {
+                case StringType.HALF_NUMBER:
+                    regexString = @"^[0-9]*$";
+                    errorString = "半角数字";
+                    break;
+                case StringType.HALF_NUMBER_DOT:
+                    regexString = @"^\d*\.*\d*$";
+                    errorString = "半角数字(小数点以下含む)";
+                    break;
+                case StringType.HALF_ALPHA_NUMBER:
+                    regexString = @"^[0-9a-zA-Z]*$";
+                    errorString = "半角英数字";
+                    break;
+                case StringType.HALF_ALPHA_NUMBER_SYMBOL:
+                    regexString = @"^[!-~]*$";
+                    errorString = "半角英数字記号";
+                    break;
+            }
+
+            Regex regex = new Regex(regexString);
+            if (!regex.IsMatch(((Control)sender).Text))
+            {
+                errorProvider.SetError((Control)sender, errorString);
+                e.Cancel = true;
+            }
+        }
+
+        private void textBox_Validated(object sender, EventArgs e)
+        {
+            errorProvider.SetError((TextBox)sender, null);
+        }
+    }
+}
